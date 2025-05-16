c5d04bbc547aef171493d2d6da6fa014a596fe1e
mohammadhoseinbaba <sirhb77@gmail.com>
2025-05-16 17:23:27 +0200

    making packages Model & repository

---
+++ src/main/java/com/example/authshop/AuthshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.authshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/authshop/AuthshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.authshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
